d3beedfb1c4c8ac11290e8f6c1ca342ea843fc35
HandFaceGender <handfacegenderprediction@gmail.com>
2025-09-17 23:22:00 +0100

    Update print_best_runs.py

--- scripts/print_best_runs.py
+++ scripts/print_best_runs.py
@@ -1,6 +1,7 @@
 import pandas as pd, typer
 from pathlib import Path
 from rich import print
+import ast
 from IPython.display import display
 app=typer.Typer(add_completion=False)
 CSV=Path("metrics/final_metrics_with_full_configurations.csv")
@@ -9,10 +10,17 @@ CSV=Path("metrics/final_metrics_with_full_configurations.csv")
 def main(k:int=10,arch:str=None,modality:str=None):
     if not CSV.exists(): raise SystemExit("metrics CSV missing")
     df=pd.read_csv(CSV)
-    if arch and "arch" in df: df=df[df["arch"].str.contains(arch,case=False,na=False)]
-    if modality and "test_modality" in df: df=df[df["test_modality"].str.contains(modality,case=False,na=False)]
-    metric="bal_acc" if "bal_acc" in df else "accuracy"
-    df=df.sort_values(metric,ascending=False).head(k)
-    display(df)
+  
+    df = pd.read_csv("metrics/final_metrics_with_full_configurations.csv")
+
+    df['configuration'] = df['configuration'].apply(ast.literal_eval)
+
+    df_metrics_configs_ablation = df[df['category'] == 'ablation']
+    df_metrics_configs_ablation.loc[:,'val_acc_mean'] = df_metrics_configs_ablation.configuration.apply(lambda x: x['mean_acc'])
+    df_flags_idx = df_metrics_configs_ablation.configuration.apply(lambda x: x['flag_on'] == True)
+
+    df_falgs_on = df_metrics_configs_ablation[df_flags_idx]
+    df_best_model = df_falgs_on.sort_values(by='val_acc_mean', ascending=False).iloc[0]
+    display(df_best_model)
 
 if __name__=="__main__": app()
